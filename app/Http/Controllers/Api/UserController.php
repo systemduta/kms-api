506c33a9d2a79f4d4728b01ce29ca474124b3cc7
Yogabayu <yogabayusbi@gmail.com>
2023-06-07 17:02:54 +0700

    TODO : Permission

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
      * 
      * fungsi: 
      *  untuk mengatur login
-     *  nik -> kalau pakai data nik
+     *  nik -> kalau pakai data nik;
      *  usernmae -> kalau pakai username
      * . dengan syarat parameter isWeb == 1
      * 
@@ -31,18 +31,25 @@ class UserController extends Controller
      *  - password
      *  - isWeb
      */
-    public function login(Request $request){
-        if(Auth::attempt([
-                'nik' => $request->nik,            
-                // 'username' => $request->username,
-                'password' => $request->password
-            ]))
-            {
+    public function login(Request $request)
+    {
+        if (Auth::attempt([
+            'nik' => $request->nik,
+            // 'username' => $request->username,
+            'password' => $request->password
+        ])) {
             $user = Auth::user();
-            $org= DB::table('organizations')->where('id', $user->organization_id)->first();
-            if($request->isWeb=="1") {
-                if($org->is_str!=1) return response()->json(['message' => 'Unauthorized'], 401);
+            $cekPermision = DB::table('permissions')
+                ->where('user_id', $user->id)
+                ->first();
+            if (!$cekPermision) {
+                return response()->json(['message' => 'Unauthorized - Not all permissions granted'], 401);
             }
+
+            // $org= DB::table('organizations')->where('id', $user->organization_id)->first();
+            // if($request->isWeb=="1") {
+            //     if($org->is_str!=1) return response()->json(['message' => 'Unauthorized'], 401);
+            // }
             $company = DB::table('companies')->where('id', $user->company_id)->first();
             $success['name'] = $user->name;
             $success['avatar'] = $user->image;
@@ -53,10 +60,14 @@ class UserController extends Controller
             $success['phone'] = $user->phone;
             $success['file'] = $user->file;
             $success['role'] = $user->role;
+            $success['isSuperAdmin'] = $cekPermision->isSuperAdmin;
+            $success['isSOP'] = $cekPermision->isSOP;
+            $success['isKMS'] = $cekPermision->isKMS;
+            $success['is1VHS'] = $cekPermision->is1VHS;
+            $success['isPAS'] = $cekPermision->isPAS;
             $success['accessToken'] = $user->createToken('nApp')->accessToken;
             return response()->json($success, $this->successStatus);
-        }
-        else {
+        } else {
             return response()->json(['message' => 'Unauthorized'], 401);
         }
     }
@@ -85,7 +96,7 @@ class UserController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+            return response()->json(['error' => $validator->errors()], 401);
         }
 
         // return response()->json(['data' => $request->nik], $this->errorStatus);
@@ -94,31 +105,31 @@ class UserController extends Controller
         /* START FILE UPLOAD */
         $file64 = $request->file;
         $ext = explode('/', explode(':', substr($file64, 0, strpos($file64, ';')))[1])[1];
-        $replace = substr($file64, 0, strpos($file64, ',')+1);
+        $replace = substr($file64, 0, strpos($file64, ',') + 1);
         $file = str_replace($replace, '', $file64);
         $file = str_replace(' ', '+', $file);
-        $filename = Str::random(10).'.'.$ext;
-        Storage::disk('public')->put('files/'.$filename, base64_decode($file));
+        $filename = Str::random(10) . '.' . $ext;
+        Storage::disk('public')->put('files/' . $filename, base64_decode($file));
         /* END FILE UPLOAD */
 
-        $username="";
-        $company=DB::table('companies')->where('id', $request->company_id)->first();
-        $username=$username.$company->code;
-        $org=DB::table('organizations')->where('id', $request->organization_id)->first();
-        $username=$username.$org->code;
-        $username=$username.$request->nik;
+        $username = "";
+        $company = DB::table('companies')->where('id', $request->company_id)->first();
+        $username = $username . $company->code;
+        $org = DB::table('organizations')->where('id', $request->organization_id)->first();
+        $username = $username . $org->code;
+        $username = $username . $request->nik;
 
-        $cek=DB::table('users')->where('nik', $request->nik)->count();
-        if($cek>0) return response()->json(['message' => 'NIK sudah terdaftar'],500);
+        $cek = DB::table('users')->where('nik', $request->nik)->count();
+        if ($cek > 0) return response()->json(['message' => 'NIK sudah terdaftar'], 500);
 
         DB::beginTransaction();
-        $userGetId=DB::table('users')->insertGetId([
+        $userGetId = DB::table('users')->insertGetId([
             'name' => $request->name,
             'password' => bcrypt($request->password),
             'company_id' => $request->company_id,
             'status' => $request->status,
             'image' => '',
-            'file' => 'files/'.$filename,
+            'file' => 'files/' . $filename,
             'organization_id' => $request->organization_id,
             'golongan_id' => $request->golongan_id,
             'nik' => $request->nik,
@@ -126,19 +137,19 @@ class UserController extends Controller
             'role' => 2
         ]);
 
-        if($request->filled('image')) {
-            $imgName='';
+        if ($request->filled('image')) {
+            $imgName = '';
             $baseString = explode(';base64,', $request->image);
             $image = base64_decode($baseString[1]);
             $image = imagecreatefromstring($image);
 
             $ext = explode('/', $baseString[0]);
             $ext = $ext[1];
-            $imgName = 'user_'.uniqid().'.'.$ext;
-            if($ext=='png'){
-                imagepng($image,public_path().'/files/'.$imgName,8);
+            $imgName = 'user_' . uniqid() . '.' . $ext;
+            if ($ext == 'png') {
+                imagepng($image, public_path() . '/files/' . $imgName, 8);
             } else {
-                imagejpeg($image,public_path().'/files/'.$imgName,20);
+                imagejpeg($image, public_path() . '/files/' . $imgName, 20);
             }
             DB::table('users')->where('id', $userGetId)->update(['image' => $imgName]);
         }
@@ -152,10 +163,10 @@ class UserController extends Controller
      */
     public function details()
     {
-        $user = User::with(['company','organization', 'golongan'])
+        $user = User::with(['company', 'organization', 'golongan'])
             ->where('id', Auth::user()->id)
             ->get();
-        return response()->json(['message' => 'success','data' => $user], $this->successStatus);
+        return response()->json(['message' => 'success', 'data' => $user], $this->successStatus);
     }
 
     /**
@@ -163,7 +174,7 @@ class UserController extends Controller
      */
     public function detailsUser($id)
     {
-        $user = User::with(['company','organization', 'golongan'])
+        $user = User::with(['company', 'organization', 'golongan'])
             ->where('id', $id)
             ->first();
         return response()->json(['success' => $user], $this->successStatus);
@@ -180,11 +191,11 @@ class UserController extends Controller
     public function index()
     {
         $auth = auth()->user();
-        $user = User::with(['company','organization', 'golongan'])
-            ->when($auth->role!=1, function ($q) use ($auth) {
+        $user = User::with(['company', 'organization', 'golongan'])
+            ->when($auth->role != 1, function ($q) use ($auth) {
                 return $q->where('company_id', $auth->company_id);
             })
-            ->where('status','!=',0)
+            ->where('status', '!=', 0)
             ->orderBy('name', 'ASC')
             ->get();
         return response()->json(['data' => $user]);
@@ -212,35 +223,35 @@ class UserController extends Controller
         if ($request->filled('file')) {
             $file64 = $request->file;
             $ext = explode('/', explode(':', substr($file64, 0, strpos($file64, ';')))[1])[1];
-            $replace = substr($file64, 0, strpos($file64, ',')+1);
+            $replace = substr($file64, 0, strpos($file64, ',') + 1);
             $file = str_replace($replace, '', $file64);
             $file = str_replace(' ', '+', $file);
-            $filename = Str::random(10).'.'.$ext;
-            Storage::disk('public')->put('files/'.$filename, base64_decode($file));
+            $filename = Str::random(10) . '.' . $ext;
+            Storage::disk('public')->put('files/' . $filename, base64_decode($file));
         }
         /* END FILE UPLOAD */
 
-        $imgName=null;
-        if($request->filled('image')) {
+        $imgName = null;
+        if ($request->filled('image')) {
             $baseString = explode(';base64,', $request->image);
-            if (count($baseString)>1) {
+            if (count($baseString) > 1) {
                 $image = base64_decode($baseString[1]);
                 $image = imagecreatefromstring($image);
 
                 $ext = explode('/', $baseString[0]);
                 $ext = $ext[1];
-                $imgName = 'user_'.uniqid().'.'.$ext;
-                if($ext=='png'){
-                    imagepng($image,public_path().'/files/'.$imgName,8);
+                $imgName = 'user_' . uniqid() . '.' . $ext;
+                if ($ext == 'png') {
+                    imagepng($image, public_path() . '/files/' . $imgName, 8);
                 } else {
-                    imagejpeg($image,public_path().'/files/'.$imgName,20);
+                    imagejpeg($image, public_path() . '/files/' . $imgName, 20);
                 }
             }
         }
 
         $user = User::find($id);
         $user->image = $imgName ?? $user->image;
-        $user->file = $request->filled('file') ? 'files/'.$filename: $user->file;
+        $user->file = $request->filled('file') ? 'files/' . $filename : $user->file;
         $user->name = $name;
         $user->nik = $nik;
         $user->company_id = $company_id;
@@ -251,10 +262,13 @@ class UserController extends Controller
         if ($request->password) $user->password = bcrypt($request->password);
         $user->save();
 
-        return response()->json([
-            'success'=>$user,
-            'message'=>'Update Successfully'],
-        $this->successStatus);
+        return response()->json(
+            [
+                'success' => $user,
+                'message' => 'Update Successfully'
+            ],
+            $this->successStatus
+        );
     }
 
     /**
---
+++ database/migrations/2023_06_07_084220_create_permissions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('isSuperAdmin')->default(false);
+            $table->boolean('isSOP')->default(false);
+            $table->boolean('isKMS')->default(false);
+            $table->boolean('is1VHS')->default(false);
+            $table->boolean('isPAS')->default(false);
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+    }
+}
